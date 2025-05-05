be42191ccffcc2e4615f46d8919746c090fbd12e
Devin Cheng <devincheng9@gmail.com>
2025-05-04 21:01:46 -0700

    train

--- train.py
+++ train.py
@@ -9,10 +9,10 @@ save_dir = 'runs'
 os.makedirs(save_dir, exist_ok=True)    
 
 # Test final learning rates with Stochastic Gradient Descent
-results = model1.train(name='train' , data='data/v1/data.yaml', epochs=80, imgsz=640, device=[0,1], project=save_dir)
+results = model1.train(name='train' , data='data/v1/data.yaml', epochs=80, imgsz=640, device=[0,1,2,3], project=save_dir, optimizer='SGD')
 
-#results = model1.train(name='AdamW_lrf1',data='v1/data.yaml', epochs=100, imgsz=640, device=[0], project=save_dir, optimizer='AdamW', lr0=0.03, lrf=1, patience = 100)
-# Test final learning rates with AdamW
+#results = model1.train(name='AdamW_lrf1',data='v1/data.yaml', epochs=100, imgsz=640, device=[0], project=save_dir, optimizer='AdamW')
+
 #results = model4.train(name='AdamW_lrf1'   , data='v5/data.yaml', epochs=300, imgsz=640, device=[0], optimizer='AdamW', lr0=0.03, lrf=1)
 #results = model5.train(name='AdamW_lrf0.5' , data='v5/data.yaml', epochs=300, imgsz=640, device=[0], optimizer='AdamW', lr0=0.03, lrf=0.5)
 #results = model6.train(name='AdamW_lrf0.03', data='v5/data.yaml', epochs=300, imgsz=640, device=[0], optimizer='AdamW', lr0=0.03, lrf=0.04)
